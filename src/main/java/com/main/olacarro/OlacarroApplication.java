60871fc30249e34e34aad4fbe0e16d20be1fb256
jjoliveira <thejjoliveira@gmail.com>
2019-06-02 01:34:58 +0100

    uploading initial project files from https://start.spring.io

---
+++ src/main/java/com/main/olacarro/OlacarroApplication.java
@@ -0,0 +1,13 @@
+package com.main.olacarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlacarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlacarroApplication.class, args);
+	}
+
+}
