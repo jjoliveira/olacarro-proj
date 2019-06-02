9fceb2b7fbd83dba8ede1e4d6a92892c2436a5c4
jjoliveira <thejjoliveira@gmail.com>
2019-06-02 02:38:35 +0100

    /upload_csv endpoint done

--- src/main/java/com/main/olacarro/ListingController.java
+++ src/main/java/com/main/olacarro/ListingController.java
@@ -38,7 +38,42 @@ public class ListingController {
 	@PostMapping("/upload_csv/{dealer_id}")
     public Iterable<Listing> uploadCSV(@PathVariable(required=true) String dealer_id, @RequestParam("file") MultipartFile csv) throws IOException {
 		
+		return listingRepository.saveAll(parseCSV(dealer_id, csv));
     }
 
+	private List<Listing> parseCSV(String dealer_id, MultipartFile csv) throws IOException {
+		
+		List<Listing> retList = new ArrayList<Listing>();
+		
+		File tempFile = File.createTempFile("temp", ".csv");
+		
+		csv.transferTo(tempFile);
+		
+		BufferedReader br = new BufferedReader(new FileReader(tempFile));
+        String line = br.readLine(); //reads header line
+            
+        while ((line = br.readLine()) != null) {
+        	retList.add(parseLine(dealer_id, line));    
+        }
+        br.close();
+		return retList;
+	}
+
+	private Listing parseLine(String dealer_id, String line) {
+		String[] aux = line.split(",");
+		
+		String code = aux[0];
+		String make = aux[1].split("/")[0];
+		String model = aux[1].split("/")[1]; 
+		int kW = Integer.parseInt(aux[2]);
+		int year = Integer.parseInt(aux[3]); 
+		String color = aux.length == 6 ? aux[4] : null; //assuming sometimes color can be missing like in the provided example
+		int price = aux.length == 6 ? Integer.parseInt(aux[5]) : Integer.parseInt(aux[4]);
+		Listing retListing = new Listing(code, make, model, kW, year, color, price);
+		
+		retListing.setDealer(dealer_id);
+		
+		return retListing;
+	}
 
 }
